deb29112c79020e66a58dc9d9a7e4af034b31987
raul <rauldearriba@gmail.com>
2020-11-16 09:07:43 +0100

    update db

--- html/db.php
+++ html/db.php
@@ -3,12 +3,10 @@ $host = "mysql-server";
 $user = "root";
 $pass = "secret";
 $db = "app1";
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connected successfully";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+$conn = mysqli_connect(host, user, pass, db);
+
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
