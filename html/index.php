42983857625aa342f1b098a938b7eb7574207a72
raul <rauldearriba@gmail.com>
2020-11-16 08:54:40 +0100

    v1 sql injection

---
+++ html/index.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+ 
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Include config file
+require_once "db.php";
+ 
+$user = $passwd = "";
+$username_err = $password_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    if(empty(trim($_POST["user"]))){
+      $username_err = "Please enter username.";
+    } else{
+      $user = trim($_POST["user"]);
+    }
+    
+    if(empty(trim($_POST["passwd"]))){
+      $password_err = "Please enter your password.";
+    }else{
+      $passwd = trim($_POST["passwd"]);
+    }
+    
+    if(empty($username_err) && empty($password_err)){
+      $sql = "SELECT id, user, passwd FROM users WHERE user='$user' AND passwd='$passwd'";
+
+      $q = mysqli_execute($conn, $sql);
+      if(!q){
+        echo "Oops! Something went wrong.";  
+      }else{
+        session_start();
+                            
+        $_SESSION["loggedin"] = true;
+        $_SESSION["id"] = $id;
+        $_SESSION["username"] = $username;                            
+                            
+        header("location: welcome.php");
+      }
+    }
+    
+    mysqli_close($link);
+}
+
+
+?>
+<html>
+<head>
+  <title> Basic SQL injection <title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <div class="container">
+      <label for="user"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="user" required>
+
+      <label for="passwd"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="passwd" required>
+
+      <button type="submit">Login</button>
+    </div>
+
+    <div>
+      <button type="button">Cancel</button>
+    </div>
+  </form>
+</body>
+</html>
---
+++ html/welcome.php
@@ -0,0 +1,26 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+    <div>
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p>
+        <a href="logout.php">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
